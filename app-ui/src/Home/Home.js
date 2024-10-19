fa871f701ce560467a3b83a3b52966cb43c71756
cwang12197 <wchristina4@gmail.com>
2024-10-19 16:28:07 -0400

    add slide transition

--- app-ui/src/Home/Home.js
+++ app-ui/src/Home/Home.js
@@ -1,28 +1,50 @@
-import React, { useRef, useState } from 'react';
-import '../App/App.css';
-import ArrowAnimation from '../ArrowAnimation';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { motion, useAnimation } from 'framer-motion';
 
+import ArrowAnimation from '../ArrowAnimation';
+import '../App/App.css';
 
 function Home() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
+  const controls = useAnimation();
 
-const handleHomeArrowClick = () => {
-        navigate('/mood');
-      };
+  const handleHomeArrowClick = () => {
+    controls.start('out');
+  };
+
+  const pageVariants = {
+    initial: {
+      y: 0,
+    },
+    out: {
+      y: '-100%', 
+      transition: {
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+  };
 
   return (
-    <div className="App">
-      
+    <motion.div
+      className="App"
+      initial="initial"
+      animate={controls}
+      variants={pageVariants}
+      onAnimationComplete={(definition) => {
+        if (definition === 'out') {
+          navigate('/mood');
+        }
+      }}
+    >
       <header className="App-header">
         <h1 className="logo">FEELIFY</h1>
         <h2 className="subtitle">YOUR EMOTIONS, YOUR PLAYLIST</h2>
-        <p className="description">
-          INSTANTLY CREATE MUSIC THAT RESONATES
-        </p>
+        <p className="description">INSTANTLY CREATE MUSIC THAT RESONATES</p>
         <ArrowAnimation handleClick={handleHomeArrowClick} />
       </header>
-    </div>
+    </motion.div>
   );
 }
 
