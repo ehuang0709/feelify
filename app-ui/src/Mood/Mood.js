1188f8e6259c4ca03627e373590e922231a607d5
cwang12197 <wchristina4@gmail.com>
2024-10-19 12:04:40 -0400

    add draggable arrow

--- app-ui/src/Mood/Mood.js
+++ app-ui/src/Mood/Mood.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './Mood.css';
 
 function Mood() {
   return (
     <div className="mood-container">
-      <h1>mood page</h1>
+      <h1>Mood Page</h1>
+      <motion.div
+        className="draggable-arrow"
+        drag
+        dragMomentum={false} 
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <div className="triangle"></div>
+      </motion.div>
     </div>
   );
 }
