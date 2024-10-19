67c38dd57fd4754d45173af35fad4f59a3670319
ehuang0709 <ehuang0709@gmail.com>
2024-10-19 19:47:41 -0400

    attempt fix cors pt. 9

--- app-backend/server.py
+++ app-backend/server.py
@@ -19,7 +19,9 @@ REDIRECT_URI = 'https://feelify.netlify.app/playlist'
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'https://feelify.netlify.app'
+    origin = request.headers.get('Origin')
+    if origin in ["https://feelify.netlify.app", "https://the-repo.onrender.com"]:
+        response.headers['Access-Control-Allow-Origin'] = origin
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
