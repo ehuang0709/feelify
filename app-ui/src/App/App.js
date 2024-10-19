74873ef57ac67e09147f08fbe73f572e7f0168c3
cwang12197 <wchristina4@gmail.com>
2024-10-19 11:51:40 -0400

    create scrollable section after click

--- app-ui/src/App/App.js
+++ app-ui/src/App/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+
+import React, { useRef, useState } from 'react';
 import './App.css';
 import ArrowAnimation from '../ArrowAnimation';
+import Home from '../Home/Home';
 
 function App() {
+  const [showHome, setShowHome] = useState(false);
+  const homeRef = useRef(null);
+
+  const handleArrowClick = () => {
+    setShowHome(true);
+    setTimeout(() => {
+      if (homeRef.current) {
+        window.scrollTo({
+          top: homeRef.current.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    }, 100);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +28,13 @@ function App() {
         <p className="description">
           INSTANTLY CREATE MUSIC THAT RESONATES
         </p>
-        <ArrowAnimation />
+        <ArrowAnimation handleClick={handleArrowClick} />
       </header>
+      {showHome && (
+        <div ref={homeRef}>
+          <Home />
+        </div>
+      )}
     </div>
   );
 }
