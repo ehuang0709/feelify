2aa3689bf796e524013dd2ce5af3c7f0a0d94353
cindyd02 <127247829+cindyd02@users.noreply.github.com>
2024-10-19 18:31:43 -0400

    fixed pre auth page

--- app-ui/src/PreAuthenticationScreen/PreAuthenticationScreen.js
+++ app-ui/src/PreAuthenticationScreen/PreAuthenticationScreen.js
@@ -28,22 +28,27 @@ function PreAuthenticationScreen() {
 
   return (
     <div className="pre-auth-container">
-      <h1 className="logo">FEELIFY</h1>
-      <h2 className="subtitle">PLAYLIST</h2>
-      <div className="spotify-logo">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="Spotify Logo" />
+      <div className="upper-content">
+        <h1 className="logo">FEELIFY</h1>
+        <h2 className="subtitle">PLAYLIST</h2>
+        <div className="spotify-logo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="Spotify Logo" />
+        </div>
       </div>
-      <p className="description">
-        CONNECT YOUR SPOTIFY ACCOUNT SO FEELIFY PLAYLIST CAN DISCOVER YOUR MUSIC TASTE.
-      </p>
-      <p className="policy">
-        By connecting you confirm that you have read and understood the <a href="https://www.spotify.com/us/legal/privacy-policy/">Privacy & Cookie Policy </a>  
-        and agree to the processing of your data and the use of cookies in accordance with it.
-      </p>
-      <button className="connect-button" onClick={authenticate}>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg" alt="Spotify Icon" />
-        CONNECT WITH SPOTIFY
-      </button>
+      
+      <div className="lower-content">
+        <p className="description">
+          CONNECT YOUR SPOTIFY ACCOUNT SO FEELIFY PLAYLIST CAN DISCOVER YOUR MUSIC TASTE.
+        </p>
+        <p className="policy">
+          By connecting you confirm that you have read and understood the <a href="https://www.spotify.com/us/legal/privacy-policy/">Privacy & Cookie Policy </a>  
+          and agree to the processing of your data and the use of cookies in accordance with it.
+        </p>
+        <button className="connect-button" onClick={authenticate}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg" alt="Spotify Icon" />
+          CONNECT WITH SPOTIFY
+        </button>
+      </div>
     </div>
   );
 }
