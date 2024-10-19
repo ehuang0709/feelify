2a4c454875bfa6316f1cccfe9fc55196fca3e9dd
cwang12197 <wchristina4@gmail.com>
2024-10-19 16:50:19 -0400

    add playlist route

--- app-ui/src/App/App.js
+++ app-ui/src/App/App.js
@@ -4,6 +4,7 @@ import Mood from "../Mood/Mood.js"
 import PreAuthenticationScreen from '../PreAuthenticationScreen/PreAuthenticationScreen.js';
 import Home from '../Home/Home.js';
 import { AnimatePresence } from 'framer-motion';
+import Playlist from "../Playlist/Playlist.js"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/mood" element={<Mood />} />
         <Route path="/pre-auth" element={<PreAuthenticationScreen />} />
+        <Route path = "/playlist" element = {<Playlist />} />
       </Routes>
       </AnimatePresence>
     </Router>
