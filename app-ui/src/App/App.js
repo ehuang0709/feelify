b6822fa75364b2f97b1783e575e0d20bfd2add43
cwang12197 <wchristina4@gmail.com>
2024-10-19 11:36:31 -0400

    fix main page description

--- app-ui/src/App/App.js
+++ app-ui/src/App/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="logo">FEELIFY</h1>
-        <h2 className="subtitle">PLAYLIST</h2>
+        <h2 className="subtitle">YOUR EMOTIONS, YOUR PLAYLIST</h2>
         <p className="description">
-          INSTANTLY MAKE A SPOTIFY PLAYLIST TO SUIT YOUR MOOD AND TASTE
+          INSTANTLY CREATE MUSIC THAT RESONATES
         </p>
         <ArrowAnimation />
       </header>
