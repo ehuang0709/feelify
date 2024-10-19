4a5055bf62e64bd91f2d8ce5b3cec0144076320d
cindyd02 <127247829+cindyd02@users.noreply.github.com>
2024-10-19 16:23:49 -0400

    authentication page

--- app-ui/src/PreAuthenticationScreen/PreAuthenticationScreen.js
+++ app-ui/src/PreAuthenticationScreen/PreAuthenticationScreen.js
@@ -4,7 +4,7 @@ import './PreAuthenticationScreen.css';
 function PreAuthenticationScreen() {
   return (
     <div className="pre-auth-container">
-      <h1 className="logo">MOOD</h1>
+      <h1 className="logo">FEELIFY</h1>
       <h2 className="subtitle">PLAYLIST</h2>
       <div className="spotify-logo">
         <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="Spotify Logo" />
@@ -13,7 +13,7 @@ function PreAuthenticationScreen() {
         CONNECT YOUR SPOTIFY ACCOUNT SO MOOD PLAYLIST CAN DISCOVER YOUR MUSIC TASTE.
       </p>
       <p className="policy">
-        By connecting you confirm that you have read and understood the <a href="#">Privacy & Cookie Policy</a> 
+        By connecting you confirm that you have read and understood the <a href="https://tools.sonymusiccreative.com/privacy/">Privacy & Cookie Policy </a>  
         and agree to the processing of your data and the use of cookies in accordance with it.
       </p>
       <button className="connect-button">
